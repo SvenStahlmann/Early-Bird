7ac00320f7ba195598e9df90706382a4c98baa20
T1mbo96 <TimoM96@outlook.de>
2020-05-31 14:50:08 +0200

    update base.css, add base.js

--- raids/static/raids/js/base.js
+++ raids/static/raids/js/base.js
@@ -6,11 +6,9 @@ function set_active_encounter() {
     if (document.getElementById('encounter-id')) {
         let encounter_id = document.getElementById('encounter-id').innerHTML;
         encounter_id = encounter_id.split('\"').join('');
-        console.log(encounter_id);
 
         if (document.getElementById('encounter-' + encounter_id)) {
             let active_encounter = document.getElementById('encounter-' + encounter_id);
-            console.log(active_encounter);
             active_encounter.parentElement.parentElement.previousElementSibling.click();
             active_encounter.classList.add('active');
         }

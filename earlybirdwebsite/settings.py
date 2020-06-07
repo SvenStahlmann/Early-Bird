3772b1aa8dbc3b0b0900f177f906f0ef32543f9d
Sven <stahlmann.sven.de@gmail.com>
2020-06-07 23:33:54 +0200

    fixed naming

--- earlybirdwebsite/settings.py
+++ earlybirdwebsite/settings.py
@@ -82,7 +82,7 @@ DATABASES_AVAILABLE = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     },
-    'develop': {
+    'deploy': {
         'ENGINE': 'django.db.backends.mysql',
         'OPTIONS': {
             'read_default_file': '/etc/mysql/my.cnf',

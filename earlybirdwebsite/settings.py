39e8624b3d79ccc48d8e130231633088751074cc
Sven <stahlmann.sven.de@gmail.com>
2020-06-09 14:29:08 +0200

    added 'ebe.svenstahlmann.de' to ALLOWED_HOSTS

--- earlybirdwebsite/settings.py
+++ earlybirdwebsite/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = ['173.249.31.162', '127.0.0.1','localhost']
+ALLOWED_HOSTS = ['173.249.31.162', '127.0.0.1','localhost','ebe.svenstahlmann.de']
 
 # Application definition
 

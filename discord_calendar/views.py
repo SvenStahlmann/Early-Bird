4c9f4a08166f05a03b3b4a8cd3c6580fa90153e0
manns <mannstahl@hotmail.de>
2020-08-27 16:10:21 +0200

    Requires super user to start discord bot.

--- discord_calendar/views.py
+++ discord_calendar/views.py
@@ -5,7 +5,12 @@ from . import bot
 
 # Create your views here.
 def start_bot(request):
+    if request.user.is_superuser:
+        bot.DiscThread()
 
-    bot.DiscThread()
+        return HttpResponse("Discord Bot started!")
 
-    return HttpResponse("Discord Bot started!")
+    else:
+        response = render(request, '404.html')
+        response.status_code = 404
+        return response

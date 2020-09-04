e8de0d3af3e69e37432c1af15e54a2202a2f0dae
manns <mannstahl@hotmail.de>
2020-09-04 08:38:30 +0200

    Added API for the calendar bot

--- discord_calendar/urls.py
+++ discord_calendar/urls.py
@@ -2,8 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('start', views.start_bot, name='start_bot'),
-
     path('api/add_raid', views.create_raid_day, name='create_raid'),
     path('api/characters', views.get_all_character_names, name='all_character'),
     path('api/entry', views.create_calendar_entry, name='cal_entry'),
